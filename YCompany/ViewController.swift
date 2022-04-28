d7fe7fb007b5edbc3809f557b5333cf72fe841fa
consultant-mayank.pahuja <consultant-mayank.pahuja@mcb.mu>
2022-04-28 23:16:36 +0530

    Initial Commit

---
+++ YCompany/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YCompany
+//
+//  Created by Mayank Pahuja on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
